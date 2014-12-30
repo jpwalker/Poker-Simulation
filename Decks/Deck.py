6ae4afe1ef5e21c1f12415e2e7910191ca1da4cc
Jean P Walker <jpwalker4039@gmail.com>
2014-12-30 17:27:52 -0500

    12-30-14

--- Decks/Deck.py
+++ Decks/Deck.py
@@ -5,6 +5,7 @@ Created on Dec 27, 2014
 '''
 
 from collections import MutableSequence
+import random as rand
 
 class Deck(MutableSequence):
     def __init__(self, seq):
@@ -25,13 +26,24 @@ class Deck(MutableSequence):
     
     def insert(self, index, value):
         self.deck.insert(index, value)
+    
+    def __repr__(self):
+        return '{0}({1})'.format(self.__class__.__name__, self.deck)
 
+    
+    def shuffle(self, seed = None):
+        if seed != None:
+            rand.seed(seed)
+        rand.shuffle(self.deck)
+        
 if __name__ == '__main__':
     # Tests for the Deck Class
     deck1 = Deck([0,1,3])
-    print(deck1.deck)
+    print(deck1)
     drawn = deck1.pop()
     print(drawn)
-    print(deck1.deck)
+    print(deck1)
     deck1.insert(0, drawn)
-    print(deck1.deck)
+    print(deck1)
+    deck1.extend([4,5,6])
+    print(deck1)
